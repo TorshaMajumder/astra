af8415f8cae703dfe23e59d6ee896d0d9e42253f
TorshaMajumder <majumder@clrlsstpc02.in2p3.fr>
2025-04-11 21:52:33 +0200

    added the mag saturation and limit values

--- dart/bands/bands.py
+++ dart/bands/bands.py
@@ -3,4 +3,7 @@ import numpy as np
 # Define the ZTF bands in Angstroms (ZTF mean filter wavelengths)
 #
 ztf_band = {'g': np.log10(4746.48), 'r': np.log10(6366.38), 'i': np.log10(7829.03)}
-ztf_mag = {'saturation':13.5, 'limit':21.0}
+#
+# The ZTF mag saturation and limit values are found based on 99% of the standardized magnitude of the largest dataset
+#
+ztf_mag = {'saturation':-0.6958509378753988, 'limit':1.5205410620126045}
